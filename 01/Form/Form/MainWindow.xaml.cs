b23f4f223c28bf1c4bb2a8fffb71e3d9ba07af17
Szymon Piórkowski <szymmon123@gmail.com>
2018-10-16 21:51:41 +0200

    Added project 01.

---
+++ 01/Form/Form/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Form
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_Clear(object sender, RoutedEventArgs e)
+        {
+            tbimie.Clear();
+            tbnazwisko.Clear();
+            dpbday.SelectedDate = null;
+            cbplec.SelectedItem = null;
+        }
+
+        private void Click_Save(object sender, RoutedEventArgs e)
+        {
+            Models.Person person = new Models.Person();
+            person.Name = tbimie.Text;
+            person.Surname = tbnazwisko.Text;
+            if (cbplec.SelectedItem != null)
+                person.Sex = (Models.Sex)cbplec.SelectedIndex;
+            if (dpbday.SelectedDate != null)
+                person.Bday = dpbday.SelectedDate ?? DateTime.Now;
+        }
+    }
+}
---
+++ 01/Form/Form/Models/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form.Models
+{
+    public enum Sex
+    {
+        MALE = 0,
+        FEMALE
+    }
+
+    class Person
+    {
+        private string name;
+        private string surname;
+        private DateTime bday;
+        private Sex sex;
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public DateTime Bday { get => bday; set => bday = value; }
+        public Sex Sex { get => sex; set => sex = value; }
+
+        public Person(string name, string surname, DateTime bday, Sex sex)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.bday = bday;
+            this.sex = sex;
+        }
+
+        public Person()
+        {
+
+        }
+
+
+    }
+}
